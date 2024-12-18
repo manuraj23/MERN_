8d22cfa0fb462de8b23ab12d4f84ed5b78caba3b
manuraj23 <manuraj082004@gmail.com>
2024-12-18 22:49:29 +0530

    Adding data to DB from a JSON file

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -5,16 +5,55 @@ const movieSchema1 = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Movie name is required'],
-        unique: true
+        unique: true,
+        trim : true
     },
-    description: String,
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        trim: true
+    },
     duration: {
         type: Number,
         required: [true, 'Duration is required']
     },
     rating: {
+        type: Number
+    },
+    totalRating: {
+        type: Number
+    },
+    releaseYear: {
         type: Number,
-        default: 1.0
+        required: [true, 'Release year is required']
+    },
+    releaseDate: {
+        type: Date,
+        required: [true, 'Release date is required']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    genre: {
+        type: [String],
+        required: [true, 'Genre is required']
+    },
+    directors: {
+        type: [String],
+        required: [true, 'Director is required']
+    },
+    coverImage: {
+        type: String,
+        required: [true, 'Cover image is required']
+    },
+    actors: {
+        type: [String],
+        required: [true, 'Actor is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required']
     }
 });
 
